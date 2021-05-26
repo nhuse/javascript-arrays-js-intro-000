ffee0438e29bc85ba77d0068e57dca95ac01d453
Nathan Huse <nathan.huse@yahoo.com>
2021-05-26 13:49:05 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snicker", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(aray, element) {
+  [element, ...array]
+  return array;
+}
+
+function 
